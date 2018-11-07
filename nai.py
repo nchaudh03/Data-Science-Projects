2ac76a7e7f0c040842745ae99616d9595c1499a1
nchaudh03 <37272716+nchaudh03@users.noreply.github.com>
2018-11-07 00:16:39 -0600

    Add files via upload

---
+++ nai.py
@@ -0,0 +1,69 @@
+
+# coding: utf-8
+
+# In[25]:
+
+
+
+import pandas as pd
+import numpy as np
+import datetime
+
+class nai():
+    now = datetime.datetime.now()
+    def read_csv(path):
+        return pd.read_csv(path)
+
+    def get_date():
+        return nai.now.strftime("%Y-%m-%d")
+    
+    def date_diff():
+        pass
+        
+    def find_null():
+        pass
+    
+    def get_sql():
+        pass
+    
+    def inner_join():
+        pass
+    
+    def left_join():
+        pass
+    
+    def union():
+        pass
+    
+    def rbind():
+        pass
+    
+    def cbind():
+        pass
+    
+    def run_reg():
+        pass
+    
+    def run_class():
+        pass
+    
+
+    
+    
+    
+    
+
+
+# In[26]:
+
+
+p = r"C:\Users\gmin4\OneDrive\Desktop\Ml Housing Data\Result.csv"
+nai.read_csv(p)
+nai.get_date()
+
+
+# In[13]:
+
+
+
+
